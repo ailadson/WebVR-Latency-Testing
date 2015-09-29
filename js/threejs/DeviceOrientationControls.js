02091b69295ab778a5cf3a6923dddecae70d83cf
ailadson <aladsonjr@gmail.com>
2015-09-28 21:55:41 -0700

    testing logger

--- js/threejs/DeviceOrientationControls.js
+++ js/threejs/DeviceOrientationControls.js
@@ -57,7 +57,7 @@ THREE.DeviceOrientationControls = function(object) {
 
   this.movementSpeed = 1.0;
   this.rollSpeed = 0.005;
-  this.autoAlign = false;
+  this.autoAlign = true;
   this.autoForward = false;
 
   this.alpha = 0;
